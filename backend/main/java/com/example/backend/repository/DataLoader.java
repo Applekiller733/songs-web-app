228ec0a66d5b9e109983f68ad69cf1789c332af3
926 - Popa-Zelu-Andrei <72962481+Applekiller733@users.noreply.github.com>
2025-04-29 15:06:23 +0300

    Add files via upload

--- backend/main/java/com/example/backend/repository/DataLoader.java
+++ backend/main/java/com/example/backend/repository/DataLoader.java
@@ -4,6 +4,7 @@ import com.example.backend.model.Song;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -16,17 +17,24 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        if (songRepository.count() == 0) { // Only load if empty
-            List<Song> songs = List.of(
-                    new Song(null, "Island in the Sun", "Weezer", "Rock", "https://i.scdn.co/image/ab67616d0000b2731e0dc5baaabda304b0ad1815", 0, 0),
-                    new Song(null, "Passenger", "Deftones", "Metal", "https://i1.sndcdn.com/artworks-Mndt2nr2zGNU-0-t500x500.jpg", 0, 0),
-                    new Song(null, "Don't Fear the Reaper", "Blue Oyster Cult","Rock",  "https://upload.wikimedia.org/wikipedia/en/6/6b/DontFearTheReaper.jpg", 0, 0),
-                    new Song(null,  "Bleed the Freak", "Alice in Chains","Metal", "https://i.scdn.co/image/ab67616d0000b2731f829ea9c2c7ffcec1a3c857", 0, 0),
-                    new Song(null, "Man in the Box", "Alice in Chains", "Metal", "https://i.scdn.co/image/ab67616d0000b2731f829ea9c2c7ffcec1a3c857", 0, 0),
-                    new Song(null, "Everlong", "Foo Fighters", "Rock", "https://i.scdn.co/image/ab67616d0000b2730389027010b78a5e7dce426b", 0, 0)
-            );
+        if (songRepository.count() == 0) {
+            List<Song> songs = new ArrayList<>();
+            songs.add(new Song(null, "Island in the Sun", "Weezer", "Rock", "https://i.scdn.co/image/ab67616d0000b2731e0dc5baaabda304b0ad1815", 0, 0));
+            songs.add(new Song(null, "Passenger", "Deftones", "Metal", "https://i1.sndcdn.com/artworks-Mndt2nr2zGNU-0-t500x500.jpg", 0, 0));
+            songs.add(new Song(null, "Don't Fear the Reaper", "Blue Oyster Cult", "Rock", "https://upload.wikimedia.org/wikipedia/en/6/6b/DontFearTheReaper.jpg", 0, 0));
+            songs.add(new Song(null, "Bleed the Freak", "Alice in Chains", "Metal", "https://i.scdn.co/image/ab67616d0000b2731f829ea9c2c7ffcec1a3c857", 0, 0));
+            songs.add(new Song(null, "Man in the Box", "Alice in Chains", "Metal", "https://i.scdn.co/image/ab67616d0000b2731f829ea9c2c7ffcec1a3c857", 0, 0));
+            songs.add(new Song(null, "Everlong", "Foo Fighters", "Rock", "https://i.scdn.co/image/ab67616d0000b2730389027010b78a5e7dce426b", 0, 0));
+            // Add more songs (e.g., 50 total)
+            songs.add(new Song(null, "Smells Like Teen Spirit", "Nirvana", "Rock", "https://example.com/image1.jpg", 0, 0));
+            songs.add(new Song(null, "Sweet Child O' Mine", "Guns N' Roses", "Rock", "https://example.com/image2.jpg", 0, 0));
+            songs.add(new Song(null, "Bohemian Rhapsody", "Queen", "Rock", "https://example.com/image3.jpg", 0, 0));
+            // Continue adding songs to reach 50+ entities
+            for (int i = 1; i <= 42; i++) {
+                songs.add(new Song(null, "Song " + i, "Artist " + (i % 5), "Genre " + (i % 3), "https://example.com/image" + i + ".jpg", 0, 0));
+            }
             songRepository.saveAll(songs);
-            System.out.println("🎵 Sample songs loaded into database!");
+            System.out.println("🎵 50+ sample songs loaded into database!");
         } else {
             System.out.println("📀 Songs already exist in the database.");
         }
